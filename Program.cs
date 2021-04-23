7fc32f46cab01569287b1b43b07b93b9b8a8c01f
pvijaykrishna549 <you@example.com>
2021-04-23 06:11:38 +0530

    First Commit:Factrial Calculation Using Recursion

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Factorial_Without_Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Input a number : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            long factorial = FactorialCalculate(n);
+            Console.WriteLine(" The factorial of {0} is : {1} ", n, factorial);
+            Console.ReadKey();
+        }
+
+        private static long FactorialCalculate(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            return n * FactorialCalculate(n - 1);
+        }
+    }
+}
